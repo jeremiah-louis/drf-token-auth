ce5c5dc8beb6572f0e56fd1c160fef2f4fc2f490
Jeremiah Louis <obobairibhojie19.jeremiah@edouniversity.edu.ng>
2024-11-09 22:10:09 +0100

    Chore: Views.py, urls.py setup

--- api/views.py
+++ api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
 # Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import permissions
+
+
+class HelloView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        content = {"message": "Hello, World!"}
+        return Response(content)
